982f89dc6c3778a4c697ce419c28a53ce86fe585
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-06-06 18:20:20 +0300

    yargs: sockfile as argument

--- bin/bdsd-cli.js
+++ bin/bdsd-cli.js
@@ -1,3 +1,17 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
+const defaultSockFile = process.env['XDG_RUNTIME_DIR'] + '/bdsd.sock';
 
-require('../index.js');
+// TODO: commands
+const argv = require('yargs')
+  .option('sockfile', {
+    alias: 's',
+    describe: `path to socket file. Default: ${process.env['XDG_RUNTIME_DIR']}/bdsd.sock'`,
+    default: defaultSockFile
+  })
+  .argv;
+
+let params = {
+  sockFile: argv['sockfile']
+};
+
+require('../index.js')(params);
--- index.js
+++ index.js
@@ -1,149 +1,178 @@
 const BdsdClient = require('bdsd.client');
 const Vorpal = require('vorpal');
 
-// BdsdClient function accepts socket filename as an argument.
-// If no argument provided then it will try to connect to following file:
-// process.env['XDG_RUNTIME_DIR'] + '/bdsd.sock'. Usually it is /run/user/1000/bdsd.sock.
 // TODO: socket file name from args
-let myClient = BdsdClient();
+
 const vorpal = Vorpal();
 
-// Register listener for broadcasted values
-myClient.on('value', data => {
-  vorpal.log('broadcasted value:', data);
-});
+const BdsdCli = function (params) {
+  // connect to IPC
+  let sockFile;
+  if (typeof params.sockFile !== "undefined") {
+    sockFile = params.sockFile;
+  }
+  const myClient = BdsdClient(sockFile);
+  // Register listener for broadcasted values
+  myClient.on('value', data => {
+    vorpal.log('broadcasted value:', data);
+  });
 
-// Listener for connected event.
-// Triggers when client connects and receive 'bus connected' notification
-myClient.on('connect', _ => {
-  vorpal.log('connected');
+  // Listener for connected event.
+  // Triggers when client connects and receive 'bus connected' notification
+  myClient.on('connect', _ => {
+    vorpal.log('connected');
+    vorpal
+      .delimiter('bobaos>')
+      .show()
+  });
+
   vorpal
-    .delimiter('bobaos>')
-    .show()
-    .parse(process.argv);
-});
-
-vorpal
-  .command('getDatapoints', 'Get all datapoint descriptions')
-  .action(function (args, callback) {
-    myClient
-      .getDatapoints()
-      .then(p => { this.log(p) })
-      .catch(e => { this.log(e) });
-    setTimeout(callback, 250);
-  });
-
-vorpal
-  .command('getDescription', 'Get datapoint description')
-  .option('-s, --start <number>', 'Required. Id of datapoint')
-  .action(function (args, callback) {
-    try {
-      if (typeof args.options.start === 'undefined') {
-        throw new Error('Please specify datapoint id');
-      }
-      let start = args.options.start;
-
-      //app.getDatapointDescription(start, number);
+    .command('getDatapoints', 'Get all datapoint descriptions')
+    .action(function (args, callback) {
       myClient
-        .getDescription(start)
-        .then(p => { this.log(p) })
-        .catch(e => { this.log(e) });
-    } catch (e) {
-      this.log(e)
-    } finally {
-      setTimeout(callback, 250);
-    }
-  });
-
-vorpal
-  .command('getValue', 'Get datapoint value')
-  .option('-s, --start <number>', 'Required. Id of datapoint')
-  .action(function (args, callback) {
-    try {
-      if (typeof args.options.start === 'undefined') {
-        throw new Error('Please specify datapoint id');
-      }
-      let start = args.options.start;
-
-      //app.getDatapointDescription(start, number);
-      myClient
-        .getValue(start)
-        .then(p => { this.log(p) })
-        .catch(e => { this.log(e) });
-    } catch (e) {
-      this.log(e)
-    } finally {
-      setTimeout(callback, 250);
-    }
-  });
-
-vorpal
-  .command('readValue', 'Send read request to bus')
-  .option('-s, --start <number>', 'Required. Id of datapoint')
-  .action(function (args, callback) {
-    try {
-      if (typeof args.options.start === 'undefined') {
-        throw new Error('Please specify datapoint id');
-      }
-      let start = args.options.start;
-
-      //app.getDatapointDescription(start, number);
-      myClient
-        .readValue(start)
-        .then(p => { this.log(p) })
-        .catch(e => { this.log(e) });
-    } catch (e) {
-      this.log(e)
-    } finally {
-      setTimeout(callback, 250);
-    }
-  });
-
-vorpal
-  .command('setValue', 'Set datapoint value')
-  .option('-s, --start <number>', 'Required. Id of datapoint')
-  .option('-v, --value <value>', 'Required. Value of datapoint')
-  .action(function (args, callback) {
-    try {
-      if (typeof args.options.start === 'undefined') {
-        throw new Error('Please specify datapoint id');
-      }
-      let start = args.options.start;
-      if (typeof args.options.value === 'undefined') {
-        throw new Error('Please specify datapoint value');
-      }
-      let value = args.options.value;
-      //app.getDatapointDescription(start, number);
-      myClient
-        .setValue(start, value)
-        .then(p => { this.log(p) })
-        .catch(e => { this.log(e) });
-    } catch (e) {
-      this.log(e)
-    } finally {
-      setTimeout(callback, 250);
-    }
-  });
-
-vorpal
-  .command('setProgrammingMode', 'Set programming mode')
-  .option('-v, --value <value>', 'Required. Value')
-  .action(function (args, callback) {
-    try {
-      let start = args.options.start;
-      if (typeof args.options.value === 'undefined') {
-        throw new Error('Please specify value');
-      }
-      let value = args.options.value;
-      myClient
-        .setProgrammingMode(value)
-        .then(_ => {
-          this.log('Set programming mode: success');
+        .getDatapoints()
+        .then(p => {
+          this.log(p)
         })
-        .catch(e => { this.log(e) });
-    } catch (e) {
-      this.log(e)
-    } finally {
+        .catch(e => {
+          this.log(e)
+        });
       setTimeout(callback, 250);
-    }
-  });
+    });
+
+  vorpal
+    .command('getDescription', 'Get datapoint description')
+    .option('-s, --start <number>', 'Required. Id of datapoint')
+    .action(function (args, callback) {
+      try {
+        if (typeof args.options.start === 'undefined') {
+          throw new Error('Please specify datapoint id');
+        }
+        let start = args.options.start;
+
+        //app.getDatapointDescription(start, number);
+        myClient
+          .getDescription(start)
+          .then(p => {
+            this.log(p)
+          })
+          .catch(e => {
+            this.log(e)
+          });
+      } catch (e) {
+        this.log(e)
+      } finally {
+        setTimeout(callback, 250);
+      }
+    });
+
+  vorpal
+    .command('getValue', 'Get datapoint value')
+    .option('-s, --start <number>', 'Required. Id of datapoint')
+    .action(function (args, callback) {
+      try {
+        if (typeof args.options.start === 'undefined') {
+          throw new Error('Please specify datapoint id');
+        }
+        let start = args.options.start;
+
+        //app.getDatapointDescription(start, number);
+        myClient
+          .getValue(start)
+          .then(p => {
+            this.log(p)
+          })
+          .catch(e => {
+            this.log(e)
+          });
+      } catch (e) {
+        this.log(e)
+      } finally {
+        setTimeout(callback, 250);
+      }
+    });
+
+  vorpal
+    .command('readValue', 'Send read request to bus')
+    .option('-s, --start <number>', 'Required. Id of datapoint')
+    .action(function (args, callback) {
+      try {
+        if (typeof args.options.start === 'undefined') {
+          throw new Error('Please specify datapoint id');
+        }
+        let start = args.options.start;
+
+        //app.getDatapointDescription(start, number);
+        myClient
+          .readValue(start)
+          .then(p => {
+            this.log(p)
+          })
+          .catch(e => {
+            this.log(e)
+          });
+      } catch (e) {
+        this.log(e)
+      } finally {
+        setTimeout(callback, 250);
+      }
+    });
+
+  vorpal
+    .command('setValue', 'Set datapoint value')
+    .option('-s, --start <number>', 'Required. Id of datapoint')
+    .option('-v, --value <value>', 'Required. Value of datapoint')
+    .action(function (args, callback) {
+      try {
+        if (typeof args.options.start === 'undefined') {
+          throw new Error('Please specify datapoint id');
+        }
+        let start = args.options.start;
+        if (typeof args.options.value === 'undefined') {
+          throw new Error('Please specify datapoint value');
+        }
+        let value = args.options.value;
+        //app.getDatapointDescription(start, number);
+        myClient
+          .setValue(start, value)
+          .then(p => {
+            this.log(p)
+          })
+          .catch(e => {
+            this.log(e)
+          });
+      } catch (e) {
+        this.log(e)
+      } finally {
+        setTimeout(callback, 250);
+      }
+    });
+
+  vorpal
+    .command('setProgrammingMode', 'Set programming mode')
+    .option('-v, --value <value>', 'Required. Value')
+    .action(function (args, callback) {
+      try {
+        let start = args.options.start;
+        if (typeof args.options.value === 'undefined') {
+          throw new Error('Please specify value');
+        }
+        let value = args.options.value;
+        myClient
+          .setProgrammingMode(value)
+          .then(_ => {
+            this.log('Set programming mode: success');
+          })
+          .catch(e => {
+            this.log(e)
+          });
+      } catch (e) {
+        this.log(e)
+      } finally {
+        setTimeout(callback, 250);
+      }
+    });
+  return myClient;
+};
+
+module.exports = BdsdCli;
